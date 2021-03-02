8ccd7e8ff4edb1f7e1bb898435e562e759676b57
FrensysThomas87 <delta873@gmail.com>
2021-03-02 21:15:30 +0100

    Aggiunta chiave "genre" a $disks

--- database/database.php
+++ database/database.php
@@ -7,70 +7,80 @@ $disks = [
     'title' => 'The dark side of the moon',
     'author' => 'Pink Floyd',
     'year' => '1973',
-    'cover' => 'img/pink-floyd.png'
+    'cover' => 'img/pink-floyd.png',
+    'genre' => 'Rock'
   ],
 
   [
     'title' => 'Pet Sounds',
     'author' => 'The Beach Boys',
     'year' => '1966',
-    'cover' => 'img/beach-boys.jpg'
+    'cover' => 'img/beach-boys.jpg',
+    'genre' => 'Pop'
   ],
 
   [
     'title' => 'Thriller',
     'author' => 'Michael Jackson',
     'year' => '1982',
-    'cover' => 'img/michael-jackson.jpg'
+    'cover' => 'img/michael-jackson.jpg',
+    'genre' => 'Pop'
   ],
 
   [
     'title' => 'Nevermind',
     'author' => 'Nirvana',
     'year' => '1991',
-    'cover' => 'img/nirvana.webp'
+    'cover' => 'img/nirvana.webp',
+    'genre' => 'Rock'
   ],
 
   [
     'title' => 'Blonde on Blonde',
     'author' => 'Bob Dylan',
     'year' => '1959',
-    'cover' => 'img/bob-dylan.jpg'
+    'cover' => 'img/bob-dylan.jpg',
+    'genre' => 'Rock'
   ],
 
   [
     'title' => 'Kind Of Blue',
     'author' => 'Miles Davis',
     'year' => '1959',
-    'cover' => 'img/miles-davis.jfif'
+    'cover' => 'img/miles-davis.jfif',
+    'genre' => 'Jazz'
   ],
 
   [
     'title' => 'London Calling',
     'author' => 'The Clash',
     'year' => '1979',
-    'cover' => 'img/London_Calling.jpg'
+    'cover' => 'img/London_Calling.jpg',
+    'genre' => 'Rock'
   ],
 
   [
     'title' => 'Who',
     'author' => 'The Who',
     'year' => '2019',
-    'cover' => 'img/the_who.jpg'
+    'cover' => 'img/the_who.jpg',
+    'genre' => 'Rock'
   ],
 
   [
     'title' => 'Low',
     'author' => 'David Bowie',
     'year' => '1977',
-    'cover' => 'img/Low.jpg'
+    'cover' => 'img/Low.jpg',
+    'genre' => 'Pop'
   ],
 
   [
     'title' => 'Born to Run',
     'author' => 'Bruce Springsteen',
     'year' => '1975',
-    'cover' => 'img/bruce.jpg'
+    'cover' => 'img/bruce.jpg',
+    'genre' => ''
   ]
 ];
 
