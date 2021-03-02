601a1aaf96a995641d993264c47de8a7bafcbed4
FrensysThomas87 <delta873@gmail.com>
2021-03-02 15:45:13 +0100

    Creato database

---
+++ database/database.php
@@ -0,0 +1,74 @@
+<?php
+$disks = [
+  [
+    'title' => 'The dark side of the moon',
+    'author' => 'Pink Floyd',
+    'year' => '1973'
+    'cover' => '../pink-floyd.png'
+  ],
+
+  [
+    'title' => 'Pet Sounds',
+    'author' => 'The Beach Boys',
+    'year' => '1966'
+    'cover' => '../beach-boys.jpg'
+  ],
+
+  [
+    'title' => 'Thriller',
+    'author' => 'Michael Jackson',
+    'year' => '1982'
+    'cover' => '../michael-jackson.jpg'
+  ],
+
+  [
+    'title' => 'Nevermind',
+    'author' => 'Nirvana',
+    'year' => '1991'
+    'cover' => '../nirvana.webp'
+  ],
+
+  [
+    'title' => 'Blonde on Blonde',
+    'author' => 'Miles Davis',
+    'year' => '1959'
+    'cover' => '../miles-davis.jfif'
+  ],
+
+  [
+    'title' => 'Kind Of Blue',
+    'author' => 'Pink Floyd',
+    'year' => '1973'
+    'cover' => '../pink-floyd.png'
+  ],
+
+  [
+    'title' => 'London Calling',
+    'author' => 'The Clash',
+    'year' => '1979'
+    'cover' => '../London_Calling.jpg'
+  ],
+
+  [
+    'title' => 'Who',
+    'author' => 'The Who',
+    'year' => '2019'
+    'cover' => '../the_who.jpg'
+  ],
+
+  [
+    'title' => 'Low',
+    'author' => 'David Bowie',
+    'year' => '1977'
+    'cover' => '../Low.jpg'
+  ],
+
+  [
+    'title' => 'Born to Run',
+    'author' => 'Bruce Springsteen',
+    'year' => '1975'
+    'cover' => '../bruce.jpg'
+  ]
+]
+
+ ?>
