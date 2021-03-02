6c77b294bfc84e581652af0984aa0c373234077c
FrensysThomas87 <delta873@gmail.com>
2021-03-02 15:58:51 +0100

    Stampati dischi a video tramite php

--- database/database.php
+++ database/database.php
@@ -3,72 +3,72 @@ $disks = [
   [
     'title' => 'The dark side of the moon',
     'author' => 'Pink Floyd',
-    'year' => '1973'
+    'year' => '1973',
     'cover' => '../pink-floyd.png'
   ],
 
   [
     'title' => 'Pet Sounds',
     'author' => 'The Beach Boys',
-    'year' => '1966'
+    'year' => '1966',
     'cover' => '../beach-boys.jpg'
   ],
 
   [
     'title' => 'Thriller',
     'author' => 'Michael Jackson',
-    'year' => '1982'
+    'year' => '1982',
     'cover' => '../michael-jackson.jpg'
   ],
 
   [
     'title' => 'Nevermind',
     'author' => 'Nirvana',
-    'year' => '1991'
+    'year' => '1991',
     'cover' => '../nirvana.webp'
   ],
 
   [
     'title' => 'Blonde on Blonde',
     'author' => 'Miles Davis',
-    'year' => '1959'
+    'year' => '1959',
     'cover' => '../miles-davis.jfif'
   ],
 
   [
     'title' => 'Kind Of Blue',
     'author' => 'Pink Floyd',
-    'year' => '1973'
+    'year' => '1973',
     'cover' => '../pink-floyd.png'
   ],
 
   [
     'title' => 'London Calling',
     'author' => 'The Clash',
-    'year' => '1979'
+    'year' => '1979',
     'cover' => '../London_Calling.jpg'
   ],
 
   [
     'title' => 'Who',
     'author' => 'The Who',
-    'year' => '2019'
+    'year' => '2019',
     'cover' => '../the_who.jpg'
   ],
 
   [
     'title' => 'Low',
     'author' => 'David Bowie',
-    'year' => '1977'
+    'year' => '1977',
     'cover' => '../Low.jpg'
   ],
 
   [
     'title' => 'Born to Run',
     'author' => 'Bruce Springsteen',
-    'year' => '1975'
+    'year' => '1975',
     'cover' => '../bruce.jpg'
   ]
-]
+];
 
  ?>
--- index.php
+++ index.php
@@ -2,3 +2,23 @@
 @require_once 'database/database.php';
 
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Php ajax dischi</title>
+   </head>
+   <body>
+     <?php foreach ($disks as $disk): ?>
+       <hr>
+
+       <div class="">
+         <h1><?php echo $disk['title'] ?></h1>
+         <h2><?php echo $disk['author'] ?></h2>
+         <h3><?php echo $disk['year'] ?></h3>
+       </div>
+
+     <?php endforeach; ?>
+   </body>
+ </html>
